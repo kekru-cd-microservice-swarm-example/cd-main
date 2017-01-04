8f0091a801c3d698ae7c96df44d1bd16c91c4d43
Kevin Krummenauer <kevin.krummenauer@adesso.de>
2017-01-04 15:07:30 +0100

    Test Jenkins-Shared Library

---
+++ src/steps/AlphaNachrichten.groovy
@@ -0,0 +1,10 @@
+#!/usr/bin/groovy
+package steps
+
+class AlphaNachrichten implements Serializable {
+  def steps
+  AlphaNachrichten(steps) {this.steps = steps}
+  def docker(args) {
+    steps.sh "./docker ${args}"
+  }
+}
