3c27b0b048ae97addc441f56d2798acae0d3f003
Kevin Krummenauer <kevin@whiledo.de>
2017-01-05 20:39:41 +0100

    compose-yml file hinzugefügt, aus dem ein docker stack erzeugt werden kann mit den Microservices

--- base-setup/base-setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker network create --attachable --driver overlay app-net
-docker run --name redis -d -p 6379:6379 --net app-net redis:alpine
-docker run --name webdis -d -p 7379:7379 --link redis:redis --net app-net anapsix/webdis
